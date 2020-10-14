739a4e0fb10d36da6bcd78aa344cea5e8545f6b3
Luca Biagetti <notciabaku@gmail.com>
2020-10-14 12:24:39 +0200

    WishMe function - v0.1

--- main.py
+++ main.py
@@ -1,2 +1,35 @@
 import pyttsx3 #pip install pyttsx3
 import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+
+print("Jarvis, avvio in corso..")
+
+master = "Luca"
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+
+    if hour>=0 and hour<12:
+        speak("Buongiorno" + master)
+
+    elif hour>=12 and hour<18:
+        speak("Buonpomeriggio" + master)
+
+    else:
+        speak("Buonasera" + master)
+    
+    speak("Sono Jarvis. Come posso aiutarti?")
+
+wishMe()
