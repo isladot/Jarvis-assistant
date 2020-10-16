7994516420f16a885f3c0f00a0be32202b629a89
Luca Biagetti <notciabaku@gmail.com>
2020-10-16 19:31:03 +0200

    Typo.

--- services/speak.py
+++ services/speak.py
@@ -1,12 +1,12 @@
 import json
+
+#IBMWatson
 from ibm_watson import TextToSpeechV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-
 authenticator = IAMAuthenticator('5_7y3FGks7xaExddpMMC859VZTDmS41LlNtgcdxv4Lqe')
 text_to_speech = TextToSpeechV1(
     authenticator=authenticator
 )
-
 text_to_speech.set_service_url('https://api.eu-de.text-to-speech.watson.cloud.ibm.com/instances/3caa2a15-1544-4f9b-adc5-8337cc6139d6')
 def watson_speak(text):
     r = random.randint(1, 1000000)
@@ -21,21 +21,20 @@ def watson_speak(text):
     playsound.playsound(file_name)
     os.remove(file_name)
 
+#Pyttsx3
 import pyttsx3
-
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
-
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+#GTTS
 import playsound
 import os
 import random
 from gtts import gTTS
-
 def google_speak(text):
     tts = gTTS(text=text, lang='it')
     r = random.randint(1, 1000000)
@@ -44,8 +43,8 @@ def google_speak(text):
     playsound.playsound(audio_file)
     os.remove(audio_file)
 
+#WindowsSystem
 import win32com.client as wincl
-
 def windows_speak(text):
     tts = wincl.Dispatch("SAPI.SpVoice")
     tts.Speak(text)
