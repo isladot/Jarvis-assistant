6f728280f1e0454b08242287cc3f3188825f170e
Luca Biagetti <notciabaku@gmail.com>
2020-10-16 21:59:22 +0200

    Config | wake property implementation.

--- main.py
+++ main.py
@@ -14,27 +14,11 @@ with open('config.json', 'r') as config:
     data = json.load(config)
     config.close()
 
-MASTER = data['master']
+WAKE = data['wake']
 TITLE = data['title']
+MASTER = data['master']
 
 print("Jarvis, avvio in corso..")
 wm.wishMe(TITLE)
 
-from commands import handler as hlr
-##
-"""
-if 'aggiorna' and 'master' in query:
-    data['master'] = 'Giovanni'
-    with open('config.json', 'w') as config:
-        json.dump(data, config)
-        config.close()
-
-elif 'apri youtube' in query:
-    url = 'youtube.com'
-    chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-    webbrowser.get(chrome_path).open_new_tab(url)
-
-elif 'che ore sono' in query:
-    strTime = datetime.datetime.now().strftime("%H:%M")
-    sp.speak(f'sono le {strTime}')
-"""
+from commands import handler
