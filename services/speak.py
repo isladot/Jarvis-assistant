297022f1ce5274c1cb03bd3eb0c33f4eced7ecf9
Luca Biagetti <notciabaku@gmail.com>
2020-10-16 02:11:19 +0200

    v0.8.1 - Typo.

--- services/speak.py
+++ services/speak.py
@@ -4,7 +4,7 @@ engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 
-def google_speak(text):
+def speak(text):
     engine.say(text)
     engine.runAndWait()
 
